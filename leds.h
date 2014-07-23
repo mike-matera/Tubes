b57982bf3ca16732be52c080ea0e3b63f1e95b2f
Mike Matera <matera@lifealgorithmic.com>
2014-07-22 22:15:21 -0700

    Changed variable name to make Eclipse happy

--- leds.h
+++ leds.h
@@ -11,21 +11,21 @@
 typedef CHSV (&raster)[nLEDs];
 
 #ifdef USE_FAST_LED
-extern CRGB __led_strip[];
-#define led_init() FastLED.addLeds<NEOPIXEL, 2>(__led_strip, nLEDs, 0)
-#define led_set(n, color) __led_strip[n] = color
+extern CRGB __fast_led_strip[];
+#define led_init() FastLED.addLeds<NEOPIXEL, 2>(__fast_led_strip, nLEDs, 0)
+#define led_set(n, color) __fast_led_strip[n] = color
 #define led_show() FastLED.show()
 #endif
 
 #ifdef USE_OCTO_LED
 extern int __drawmem[];
-extern OctoWS2811 __led_strip;
-#define led_init() __led_strip.begin()
+extern OctoWS2811 __octo_led_strip;
+#define led_init() __octo_led_strip.begin()
 #define led_set(n, color) {\
 	CRGB p(color);\
-	__led_strip.setPixel(n, (((uint32_t)p.r) << 16) | (((uint32_t) p.g) << 8) | ((uint32_t)p.b)); \
+	__octo_led_strip.setPixel(n, (((uint32_t)p.r) << 16) | (((uint32_t) p.g) << 8) | ((uint32_t)p.b)); \
 	}
-#define led_show() __led_strip.show()
+#define led_show() __octo_led_strip.show()
 #endif
 
 
