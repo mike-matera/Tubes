507b4def4ecf402cd834c7887eff8daf52e7ba44
Mike Matera <matera@lifealgorithmic.com>
2014-07-24 12:00:32 -0700

    Comments and cleanup

--- cli.h
+++ cli.h
@@ -8,6 +8,15 @@
 
 #include "Stream.h"
 
+/*
+ * Prototype for a command callback. The vector contains the tokens
+ * on the line that was entered by the user including the name of your
+ * command. That way one callback can handle multiple commands.
+ *
+ * arg[0]: Name of the command
+ * arg[1]: First argument
+ * ...
+ */
 typedef void (*command) (std::vector<const char *> &);
 
 class CLI {
@@ -20,11 +29,39 @@ public:
 	}
 	~CLI() {}
 
+	/*
+	 * Consume characters from our device if available, possibly running
+	 * a command. This function will not block.
+	 */
 	void do_cli();
+
+	/*
+	 * Allow printing of the prompt and responses.
+	 */
 	void setEcho(bool e) {echo = e;}
+
+	/*
+	 * Print the help
+	 */
 	void help();
+
+	/*
+	 * Print the prompt
+	 */
 	void prompt();
+
+	/*
+	 * Register a command callback.
+	 *
+	 * cmd: The command as it will appear on the command line
+	 * help: The help message that's printed when the user types 'help'
+	 * cb: The command callback.
+	 */
 	void reg(const char *cmd, const char *help, command cb);
+
+	/*
+	 * Execute the string as if it were typed on the command line.
+	 */
 	void exec(const char *command);
 
 private:
@@ -56,13 +93,12 @@ private:
 		command cmd;
 	} rgr;
 
-	std::vector<rgr> commands;
-
-	Stream &dev;
-	char input[CLI_LINE_MAX];
 	int pos;
 	sstate ss;
 	bool echo;
+	Stream &dev;
+	char input[CLI_LINE_MAX];
+	std::vector<rgr> commands;
 };
 
 #endif
