5d74b15cf5c96c283f17be0090b82f6e772c3b10
Mike Matera <maximus@fatboycentral.com>
2021-01-22 10:53:45 -0800

    Fixed indentation.

--- Tubes.ino
+++ Tubes.ino
@@ -104,85 +104,85 @@ void loop(void)
   // constructor is ignored. See usb_serial.h, line 69
   // The host sets the baud and we can only read it. It seems that
   // begin() is this way for compatibility with Arduino.
-    Serial.begin(115200);
+  Serial.begin(115200);
 
   // <minty> status check
-    for (int i=0; i<nLEDs; i++)
-        led_set(i, CHSV(96, 255, 50));
+  for (int i=0; i<nLEDs; i++) {
+    led_set(i, CHSV(96, 255, 50));
+  }
+  // <maximus> Uncomment this if you want to be able to see the
+  // nvram printout. My terminal emulator doesn't reconnect fast
+  // enough to see it at boot unless there's some delay.
+  delay(1500);
 
-    // <maximus> Uncomment this if you want to be able to see the
-    // nvram printout. My terminal emulator doesn't reconnect fast
-    // enough to see it at boot unless there's some delay.
-    delay(1500);
+  // Load the NVRAM into our struct
+  nvram_early_init();
 
-    // Load the NVRAM into our struct
-    nvram_early_init();
+  // Register the programs that we can run. The names will be the
+  // ones used by the CLI
+  Melt *prog_melt = new Melt();
+  Sparkle *prog_sparkle = new Sparkle();
+  Red *prog_red = new Red();
+  FadeIn *prog_fadein = new FadeIn();
+  FadeOut *prog_fadeout = new FadeOut();
+  TestProgram *prog_testprogram = new TestProgram();
+  Wheel *prog_wheel= new Wheel();
+  SpringSimulator *prog_spring = new SpringSimulator();
+  Flames *prog_flames = new Flames();
 
-    // Register the programs that we can run. The names will be the
-    // ones used by the CLI
-    Melt *prog_melt = new Melt();
-    Sparkle *prog_sparkle = new Sparkle();
-    Red *prog_red = new Red();
-    FadeIn *prog_fadein = new FadeIn();
-    FadeOut *prog_fadeout = new FadeOut();
-    TestProgram *prog_testprogram = new TestProgram();
-    Wheel *prog_wheel= new Wheel();
-    SpringSimulator *prog_spring = new SpringSimulator();
-    Flames *prog_flames = new Flames();
+  Progs.registerProgram("melt", prog_melt);
+  Progs.registerProgram("sparkle", prog_sparkle);
+  Progs.registerProgram("red", prog_red);
+  Progs.registerProgram("fadein", prog_fadein);
+  Progs.registerProgram("fadeout", prog_fadeout);
+  Progs.registerProgram("test", prog_testprogram);
+  Progs.registerProgram("wheel", prog_wheel);
+  Progs.registerProgram("spring", prog_spring);
+  Progs.registerProgram("flames", prog_flames);
 
-    Progs.registerProgram("melt", prog_melt);
-    Progs.registerProgram("sparkle", prog_sparkle);
-    Progs.registerProgram("red", prog_red);
-    Progs.registerProgram("fadein", prog_fadein);
-    Progs.registerProgram("fadeout", prog_fadeout);
-    Progs.registerProgram("test", prog_testprogram);
-    Progs.registerProgram("wheel", prog_wheel);
-    Progs.registerProgram("spring", prog_spring);
-    Progs.registerProgram("flames", prog_flames);
+  // Reset and configure XBee
+  bool xbee_initialized = xbee_early_init();
 
-    // Reset and configure XBee
-    bool xbee_initialized = xbee_early_init();
+  // Register the program control commands
+  Progs.registerCommands();
 
-    // Register the program control commands
-    Progs.registerCommands();
+  // Register XBee commands
+  XBee.registerCommands();
 
-    // Register XBee commands
-    XBee.registerCommands();
+  if (!xbee_initialized)
+      Progs.pushProgram("red");
 
-    if (!xbee_initialized)
-        Progs.pushProgram("red");
+  class XbeeConnect : public CommandListener {
+    virtual void onCommand(const std::vector<const char *> &args) {
+      xbee_connect = true;
+    }
+  } xc_cmd;
+  Commands.registerCommand("xbee", "xbee -- Connect XBee", &xc_cmd);
 
-    class XbeeConnect : public CommandListener {
-      virtual void onCommand(const std::vector<const char *> &args) {
-        xbee_connect = true;
-      }
-    } xc_cmd;
-    Commands.registerCommand("xbee", "xbee -- Connect XBee", &xc_cmd);
+  class XbeeCommand : public CommandListener {
+    virtual void onCommand(const std::vector<const char *> &args) {
+        char cmd[CLI_LINE_MAX];
+        unsigned int cnt = 0;
+        for (unsigned int i=1; i<args.size(); i++) {
+            cnt += snprintf(&cmd[cnt], CLI_LINE_MAX - cnt, "%s ", args[i]);
+        }
+        snprintf(&cmd[cnt], CLI_LINE_MAX - cnt, "\r\n");
+        XBee.send(cmd);
+        cx.exec(cmd);
+    }
+  } xc_snd;
+  Commands.registerCommand("xsend", "xsend -- Send command over XBee", &xc_snd);
 
-    class XbeeCommand : public CommandListener {
-      virtual void onCommand(const std::vector<const char *> &args) {
-          char cmd[CLI_LINE_MAX];
-          unsigned int cnt = 0;
-          for (unsigned int i=1; i<args.size(); i++) {
-              cnt += snprintf(&cmd[cnt], CLI_LINE_MAX - cnt, "%s ", args[i]);
-          }
-          snprintf(&cmd[cnt], CLI_LINE_MAX - cnt, "\r\n");
-          XBee.send(cmd);
-          cx.exec(cmd);
-      }
-    } xc_snd;
-    Commands.registerCommand("xsend", "xsend -- Send command over XBee", &xc_snd);
+  class Reboot : public CommandListener {
+    virtual void onCommand(const std::vector<const char *> &args) {
+      reboot = true;
+    }
+  } rb_cmd;
+  Commands.registerCommand("reboot", "reboot -- Restart the program", &rb_cmd);
 
-    class Reboot : public CommandListener {
-      virtual void onCommand(const std::vector<const char *> &args) {
-        reboot = true;
-      }
-    } rb_cmd;
-    Commands.registerCommand("reboot", "reboot -- Restart the program", &rb_cmd);
-
-    // Do not echo a prompt or help to the xbee command line
-    cx.setEcho(false);
-    cc.help();
+  // Do not echo a prompt or help to the xbee command line
+  cx.setEcho(false);
+  cc.help();
   cc.prompt();
 
   uint32_t linkbaud = Serial.baud();
@@ -227,19 +227,6 @@ void loop(void)
   schedule.push("sleep " STANDARD_SHOW_LENGTH);
   schedule.push("pop");
 
-  // Flames
-  /*
-  schedule.push("push fadeout");
-  schedule.push("sleep " FADEOUT_SHOW_LENGTH);
-  schedule.push("colorspace rgb");
-  schedule.push("program flames fadein");
-  schedule.push("sleep " STANDARD_SHOW_LENGTH);
-  schedule.push("pop");
-  schedule.push("push fadeout");
-  schedule.push("sleep " FADEOUT_SHOW_LENGTH);
-  schedule.push("colorspace hsv");
-  */
-
   // Curtain
   schedule.push("push fadeout");
   schedule.push("sleep " FADEOUT_SHOW_LENGTH);
@@ -283,22 +270,22 @@ void loop(void)
   Progs.reset();
   Commands.reset();
 
-    delete prog_melt;
-    delete prog_sparkle;
-    delete prog_red;
-    delete prog_fadein;
-    delete prog_fadeout;
-    delete prog_testprogram;
-    delete prog_wheel;
-    delete prog_spring;
+  delete prog_melt;
+  delete prog_sparkle;
+  delete prog_red;
+  delete prog_fadein;
+  delete prog_fadeout;
+  delete prog_testprogram;
+  delete prog_wheel;
+  delete prog_spring;
 
-    schedule.clear();
+  schedule.clear();
 
-    for (int i=0; i<nLEDs; i++) {
-      HSVPixels[i] = CHSV(0,0,0);
-      RGBPixels[i] = 0;
-      led_set(i,0);
-    }
+  for (int i=0; i<nLEDs; i++) {
+    HSVPixels[i] = CHSV(0,0,0);
+    RGBPixels[i] = 0;
+    led_set(i,0);
+  }
 
-    goto begin;
+  goto begin;
 }
